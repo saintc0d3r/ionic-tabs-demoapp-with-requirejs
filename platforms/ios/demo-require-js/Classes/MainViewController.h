c511a96c98541cf2f9236f278da42d9169f97e75
Wendy Sanarwanto <saintc0d3r@gmail.com>
2015-08-16 14:48:11 +0800

    Fixed error when running on android emulator

--- platforms/ios/demo-require-js/Classes/MainViewController.h
+++ platforms/ios/demo-require-js/Classes/MainViewController.h
@@ -19,7 +19,7 @@
 
 //
 //  MainViewController.h
-//  HelloCordova
+//  demo-require-js
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
